96abb12fed1074d24334a5db523ce5131aae7403
MoAtefAmer <mo.atefamer@gmail.com>
2025-02-14 22:06:54 +0200

    refactor: remove searchbar extra wrapper & header styling rework

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
-import {Appbar} from 'react-native-paper';
-import {Image, StyleSheet, View, Dimensions} from 'react-native';
-import {theme} from '../constants/theme';
-import SearchBar from './SearchBar';
-
-const {width} = Dimensions.get('window');
+import { Image, StyleSheet, View, StatusBar } from 'react-native';
+import { theme } from '../constants/theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useMemo } from 'react';
+import { Searchbar } from 'react-native-paper';
 
 interface HeaderProps {
   placeholder: string;
@@ -16,20 +15,30 @@ const Header: React.FC<HeaderProps> = ({
   searchQuery,
   setSearchQuery,
 }: HeaderProps) => {
+  const insets = useSafeAreaInsets();
+
+  const headerContainerStyle = useMemo(
+    () => [styles.header, { paddingTop: insets.top }],
+    [insets.top],
+  );
+
   return (
-    <View style={styles.headerContainer}>
-      <Appbar.Header style={styles.header}>
-        <Image
-          source={require('../assets/nasdaqlogo.png')}
-          style={styles.logo}
-          resizeMode="contain"
-        />
-      </Appbar.Header>
+    <View style={headerContainerStyle}>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      <Image
+        source={require('../assets/nasdaqlogo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
       <View style={styles.searchbarContainer}>
-        <SearchBar
+        <Searchbar
           placeholder={placeholder}
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
+          onChangeText={setSearchQuery}
+          value={searchQuery ?? ''}
         />
       </View>
     </View>
@@ -42,30 +51,21 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   headerContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    width: width,
     backgroundColor: theme.colors.primary,
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
-    overflow: 'hidden',
   },
   header: {
     backgroundColor: theme.colors.primary,
-    elevation: 0,
-    borderBottomWidth: 0,
-    width: '100%',
-    height: 100,
-    justifyContent: 'flex-start',
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
+    marginBottom: 10,
   },
   logo: {
     height: 40,
     width: 120,
-    alignSelf: 'center',
+    alignSelf: 'flex-start',
+    marginBottom: 15,
   },
 });
 
